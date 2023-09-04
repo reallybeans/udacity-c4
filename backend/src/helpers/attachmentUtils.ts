59dcd8441b3b4ad08994ea3bb637f53d17dd6fe7
Bean <leeanh764@gmail.com>
2023-09-05 00:26:00 +0700

    implement create todos

---
+++ backend/src/helpers/attachmentUtils.ts
@@ -0,0 +1,28 @@
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+// TODO: Implement the fileStogare logic
+const s3_bucket_name = process.env.ATTACHMENTS_S3_BUCKET
+const url_exp = process.env.SIGNED_URL_EXPIRATION
+
+export class AttachmentUtils {
+  constructor(
+    private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
+    private readonly bucket_name = s3_bucket_name
+  ) {}
+
+  getAttachmentUrl(todo_id: string) {
+    return `https://${this.bucket_name}.s3.amazonaws.com/${todo_id}`
+  }
+
+  getUploadUrl(todo_id: string) {
+    const url = this.s3.getSignedUrl('putObject', {
+      Bucket: this.bucket_name,
+      Key: todo_id,
+      Expires: url_exp
+    })
+    return url as string
+  }
+}
--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,15 +3,17 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
+import { getUserId } from '../utils'
 import { createTodo } from '../../businessLogic/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
-
-    return undefined
+    const user_id = getUserId(event)
+    const new_item = await createTodo(newTodo, user_id)
+    return { statusCode: 201, body: JSON.stringify({ item: new_item }) }
+  }
 )
 
 handler.use(
