2ec68dec63ac3245b60018f9bf2df4dd34a7726c
Bean <leeanh764@gmail.com>
2023-09-06 23:27:09 +0700

    config path

--- backend/src/dataLayer/todosAccess.ts
+++ backend/src/dataLayer/todosAccess.ts
@@ -1,10 +1,10 @@
-import * as AWS from 'aws-sdk'
 // import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate'
 var AWSXRay = require('aws-xray-sdk')
+var AWS = require('aws-sdk')
 const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('TodosAccess')
